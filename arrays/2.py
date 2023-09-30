e0c4584a59be5995681efb14f214b0625fda0a89
Eshita Islam <islam.eshita@gmail.com>
2023-09-30 09:41:07 -0700

    Changes with solution

---
+++ arrays/2.py
@@ -0,0 +1,6 @@
+import numpy as np
+a = np.array([1, 6, 3])
+
+result = a.sum()
+
+print(result)
--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,11 @@
+import numpy as np
+a = np.array([1, 6, 3])
+print(a)
 
+
+def multiply(n):
+    return n * 3
+
+
+b = np.array(list(map(multiply, a)))
+print(b)
